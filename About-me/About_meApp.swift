f6540af3ea10cd379142b805c64ba11400f7f57f
jerry-macbook <unclefish7@outlook.com>
2024-11-05 16:37:32 +0800

    Initial Commit

---
+++ About-me/About_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_meApp.swift
+//  About-me
+//
+//  Created by 贾文超 on 2024/11/5.
+//
+
+import SwiftUI
+
+@main
+struct About_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
